abd2b3723d4ed3152593b3021d9287524b08f90f
braqn1999 <117543750+braqn1999@users.noreply.github.com>
2025-05-28 22:11:58 -0500

    Day15: example

---
+++ day15-training-week1-2/num_1-20_fizz_buzz.py
@@ -0,0 +1,8 @@
+for i in range(1, 21):
+    if i % 3 == 0:
+        print(f"{i} Fizz")
+    elif i % 5 == 0:
+        print(f"{i} Buzz")
+    else:
+        print(i)
+
---
+++ day15-training-week1-2/string_in_text.py
@@ -0,0 +1,6 @@
+
+text = "Hello, I'm Jony, Nice to meet you! How are you today?"
+
+count_a = text.count("a")
+
+print(count_a)
